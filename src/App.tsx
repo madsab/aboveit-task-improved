8dacea9d5acc5e5cdd8b30b31b7b1f7e5c585623
madsab <mads.barnes@hotmail.com>
2023-10-04 11:27:18 +0200

    fetch data and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Searchbar from "./components/Searchbar";
+import CatCard from "./components/CatCard";
 
 function App() {
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.thecatapi.com/v1/breeds?limit=100&page=0&api_key=live_HQ2p2jHlCzLlVWK1G8ZjfFbqZWzBHewj6VhHfrDpdx5DLCVO58PCKRFrq8kYJLdZ"
+        );
+        const result = await response.json();
+
+        setCats(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <>
-      <div>Oppgave</div>
-    </>
+    <div className="layout">
+      <h1>Find your Dream Cat</h1>
+      <Searchbar />
+      <main className="displayCats">
+        <>
+          {loading
+            ? "Loading..."
+            : cats.map((cat, index) => {
+                return <CatCard key={index} {...cat} />;
+              })}
+        </>
+      </main>
+    </div>
   );
 }
 

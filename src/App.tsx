f20792436dc0e569e5bcd90f89d3a54da401bc5c
madsab <mads.barnes@hotmail.com>
2023-10-04 13:57:11 +0200

    filter on searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CatCard, { CatProps } from "./components/CatCard";
 function App() {
   const [cats, setCats] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [inputFilter, setInputFilter] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,7 +15,7 @@ function App() {
           "https://api.thecatapi.com/v1/breeds?limit=100&page=0&api_key=live_HQ2p2jHlCzLlVWK1G8ZjfFbqZWzBHewj6VhHfrDpdx5DLCVO58PCKRFrq8kYJLdZ"
         );
         const result = await response.json();
-
+        console.log(result);
         setCats(result);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -25,17 +26,26 @@ function App() {
 
     fetchData();
   }, []);
+
+  const handleInput = (value: string) => {
+    setInputFilter(value);
+  };
   return (
     <div className="layout">
       <h1>Find your Dream Cat</h1>
-      <Searchbar />
+      <Searchbar onChange={handleInput} />
       <main className="displayCats">
+        <div className="moreInfo"></div>
         <>
           {loading
             ? "Loading..."
-            : cats.map((cat: CatProps, index) => {
-                return <CatCard key={index} {...cat} />;
-              })}
+            : cats
+                .filter((cat: CatProps) =>
+                  cat.name.toLowerCase().includes(inputFilter)
+                )
+                .map((cat: CatProps, index) => {
+                  return <CatCard key={index} {...cat} />;
+                })}
         </>
       </main>
     </div>

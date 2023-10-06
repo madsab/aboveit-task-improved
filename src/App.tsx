df73b1fd4e136b5f1574ea33ef6550a247739222
madsab <mads.barnes@hotmail.com>
2023-10-06 21:49:07 +0200

    add: show more info on click

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Searchbar from "./components/Searchbar";
 import CatCard, { CatProps } from "./components/CatCard";
+import MoreInfo from "./components/MoreInfo";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 function App() {
   const [cats, setCats] = useState([]);
   const [loading, setLoading] = useState(true);
   const [inputFilter, setInputFilter] = useState("");
   const [noSearch, setNoSearch] = useState("");
+  const [moreInfo, setMoreInfo] = useState(null);
+  const [displayInfo, setDisplayInfo] = useState("noShow");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,16 +34,29 @@ function App() {
   const handleInput = (value: string) => {
     setInputFilter(value.toLowerCase());
     cats.filter((cat: CatProps) => cat.name.toLowerCase().includes(inputFilter))
-      .length === 0
-      ? setNoSearch("No Options")
-      : setNoSearch("");
+      .length === 0 && setNoSearch("No Options");
+  };
+
+  const showMoreInfo = (event: EventTarget) => {
+    if (event instanceof HTMLImageElement) {
+      const chosenCat = event.alt;
+      displayInfo == "noShow"
+        ? setDisplayInfo("Show")
+        : setDisplayInfo("noShow");
+      setMoreInfo(
+        cats.filter((cat: CatProps) => cat.name.includes(chosenCat))[0]
+      );
+    }
   };
   return (
     <div className="layout">
       <h1>Find your Dream Cat</h1>
       <Searchbar onChange={handleInput} />
-      <main id="main" className="displayCats">
-        <div className="moreInfo"></div>
+      <main
+        id="main"
+        className="displayCats"
+        onClick={(e) => showMoreInfo(e.target)}
+      >
         <>
           {loading
             ? "Loading..."
@@ -52,6 +69,17 @@ function App() {
                 })}
           {noSearch}
         </>
+        {displayInfo && (
+          <div className={displayInfo}>
+            <Icon
+              icon={"ph:x"}
+              width={40}
+              onClick={() => setDisplayInfo("noShow")}
+              style={{ cursor: "pointer", padding: "3px 3px" }}
+            />
+            <MoreInfo cat={moreInfo} />
+          </div>
+        )}
       </main>
     </div>
   );

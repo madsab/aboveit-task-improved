0a6864fbaeede1702a573771dc7508d05189bb82
madsab <mads.barnes@hotmail.com>
2023-10-06 20:45:41 +0200

    add: MoreInfo showing cat info

---
+++ src/components/MoreInfo.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { CatProps } from "./CatCard";
+
+interface IMoreInfoProps {
+  cat: CatProps | undefined;
+}
+
+const MoreInfo: React.FC<IMoreInfoProps> = ({ cat }) => {
+  return (
+    <div className="moreInfo">
+      <h2>{cat?.name}</h2>
+      <img src={cat?.image?.url} alt={cat?.name} width={200} />
+      <section className="description">
+        <p>{cat?.description}</p>
+        <div className="score">
+          <p>Experimental: {cat?.experimental}</p>
+          <p>Hairless: {cat?.hairless}</p>
+          <p>Hypoallergenic: {cat?.hypoallergenic}</p>
+          <p>Natural {cat?.natural}</p>
+          <p>Rare: {cat?.rare}</p>
+          <p>Short legs: {cat?.short_legs}</p>
+          <p>Social needs: {cat?.social_needs}</p>
+          <p>Stranger Friendly: {cat?.stranger_friendly}</p>
+          <p>Supressed tail: {cat?.suppressed_tail}</p>
+          <p>Temperament: {cat?.temperament}</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MoreInfo;
